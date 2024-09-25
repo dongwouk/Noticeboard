541df78fb71bddf44c3812ec41f1f1016e4c2c63
DESKTOP-8MML8S9\kwond <kwondw30@gmail.com>
2024-09-25 10:53:34 +0900

    test

--- src/main/java/com/springboot/noticeboard/config/CorsMvcConfig.java
+++ src/main/java/com/springboot/noticeboard/config/CorsMvcConfig.java
@@ -11,7 +11,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000");
-        // 동욱 test
-        System.out.println("test");
+        System.out.println("ddddd");
     }
+
 }
